10ae7425a7a405a1f2f0075a6fc0e47e5ceaaca1
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-11 00:02:08 -0200

    refactory in curry

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pagination parser link
 
+[![Build Status](https://travis-ci.org/willyamalmeida/pagination-link-parser.svg?branch=master)](https://travis-ci.org/willyamalmeida/pagination-link-parser) [![Maintainability](https://api.codeclimate.com/v1/badges/3d9710cf5d0b03584cc7/maintainability)](https://codeclimate.com/github/willyamalmeida/pagination-link-parser/maintainability)
--- src/libs/curry.js
+++ src/libs/curry.js
@@ -1,16 +1,26 @@
 "use strict";
 
-const curry = fn => {
-    if (fn == null) {
-        throw("Function not provided");
+const assertIsNull = function(value, message){
+    if (value == null) {
+        throw(message);
     }
+}
+
+const assertFunctionNotProvided = function(fn) {
+    assertIsNull(fn, "Function not provided");
+};
+
+const assertNoArguments = function(value) {
+    assertIsNull(value, "No Arguments");
+}
+
+const curry = fn => {
+    assertFunctionNotProvided(fn);
 
     let argLength = fn.length;
 
     return function curryOne(a) {
-        if (a == null) {
-            throw("No Arguments");
-        }
+        assertNoArguments(a);
 
         if (argLength - 1 === 0) {
              return fn(a);
